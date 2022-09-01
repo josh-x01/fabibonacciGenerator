7365c6ca7c3549cc1677f9ebcf1ed2d44cf853f4
Joshua <87242555+eyasuyid@users.noreply.github.com>
2022-09-01 10:01:11 +0300

    Create fabibonacciGenerator.js

---
+++ fabibonacciGenerator.js
@@ -0,0 +1,16 @@
+/*
+This program calculate fibonacci sequence
+Author: Eyasu Yidnekachew
+E-mail: eyasuyid@gmail.com
+*/
+
+function fabibonacciGenerator(n) {
+    var fabibonacciList = [];
+    for (var i = 0; i < n ; i++) {
+        if (i < 2) fabibonacciList.push(i);
+        else fabList.push(fabibonacciList[i-2] + fabibonacciList[i - 1]);      
+    } return fabibonacciList;
+}
+
+// sample
+console.log(fabibonacciGenerator(10));
